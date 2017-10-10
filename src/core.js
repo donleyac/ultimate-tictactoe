20913ed819b68b7e1a0a409b40e8f49d06fbf570
Alex Donley <donleyac@gmail.com>
2017-10-09 21:47:39 -0400

    Added Relevant Actions
    New Home Route, 
    Cleaning up core logic

--- src/core.js
+++
@@ -1,9 +0,0 @@
-import {List} from 'immutable';
-
-export function setCollectionPosition(state, collect,position,size){
-  return state.updateIn(
-    ["collections", collect, "pos"],
-    0,
-    value => List(position)
-  );
-}
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import {HashRouter as Router, Route} from 'react-router-dom'
-import Home from './Pages/Home/index.js';
-import Config from './Pages/Config/index.js';
-import GameContainer from './Pages/Game/index.js';
+import Home from './pages/Home.js';
 export default(
     <Router>
         <div>
             <Route exact path='/' component={Home}/>
-            <Route path='/config' component={Config}/>
-            <Route path='/game' component={GameContainer}/>
         </div>
     </Router>
 );
