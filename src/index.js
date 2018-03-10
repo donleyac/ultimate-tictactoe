019b4722d917b5009db8709a191010f7b1bb1888
Alex Donley <donleyac@gmail.com>
2018-03-10 16:14:21 -0500

    Fixed Added proxy to allow for correct backend calls

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import routes from  './routes';
+import './index.css';
 
 // Apollo
 import { ApolloProvider } from 'react-apollo'
